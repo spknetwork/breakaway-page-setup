708593e4980b467393bd08f7c59df7572ed7f216
igormuba <igorm-uba@hotmail.com>
2023-10-29 14:03:24 -0300

    multiple communities single docker compose

--- src/pages/DockerSetup.jsx
+++ src/pages/DockerSetup.jsx
@@ -5,13 +5,50 @@ import { FaCopy } from "react-icons/fa";
 import "./docker-setup.scss";
 
 const DockerSetup = () => {
+  const [containerEntries, setContainerEntries] = useState([]); // Store multiple container entries
   const [containerName, setContainerName] = useState("");
   const [port, setPort] = useState("");
   const [HIVE_ID, setHiveId] = useState("");
   const [TAGS, setTags] = useState("");
-  const [dockerComposeConfig, setDockerComposeConfig] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
+  const [dockerComposeConfig, setDockerComposeConfig] = useState("");
+
+  const handleAddContainer = () => {
+    if (containerName && HIVE_ID && TAGS) {
+      const newEntry = {
+        containerName,
+        port: port || "3000",
+        HIVE_ID,
+        TAGS,
+      };
+
+      setContainerEntries([...containerEntries, newEntry]); // Add the new entry to the list
+      setContainerName("");
+      setPort("");
+      setHiveId("");
+      setTags("");
+    } else {
+      setSuccessMessage("Please fill out all fields.");
+    }
+    handleGenerateCompose();
+  };
+
+  const handleGenerateCompose = () => {
+    if (containerEntries.length === 0) {
+      setSuccessMessage("Add at least one container entry.");
+      return;
+    }
+
+    const composeConfig = `version: '3'\nservices:\n${containerEntries
+      .map(
+        (entry, index) =>
+          `  container${index}:\n    image: pspc/ecency-boilerplate:legacy\n    ports:\n      - "${entry.port}:3000"\n    environment:\n      - USE_PRIVATE=1\n      - HIVE_ID=${entry.HIVE_ID}\n      - TAGS=${entry.TAGS}\n    restart: always`
+      )
+      .join("\n")}`;
+
+    setSuccessMessage("Docker Compose configuration generated.");
+    setDockerComposeConfig(composeConfig);
+  };
 
   const handleCopyToClipboard = () => {
     navigator.clipboard.writeText(dockerComposeConfig).then(() => {
@@ -19,37 +56,19 @@ const DockerSetup = () => {
     });
   };
 
-  const handleDownload = () => {
-    const config = `version: '3'
-services:
-  ${containerName}:
-    image: pspc/ecency-boilerplate:legacy
-    ports:
-      - "${port || "3000"}:3000"
-    environment:
-      - USE_PRIVATE=1
-      - HIVE_ID=${HIVE_ID}
-      - TAGS=${TAGS}
-    restart: always`;
-
-    setDockerComposeConfig(config);
-    setSuccessMessage("Docker Compose configuration generated.");
-  };
-
   return (
     <div className="docker-setup">
       <div className="docker-setup-container">
         <div className="header">Docker Container Setup</div>
-        <div className="error-message">{errorMessage}</div>
         <div className="success-message">{successMessage}</div>
 
         <div className="form-wrapper">
-          <input
+          {/* <input
             type="text"
             placeholder="Container Name"
             value={containerName}
             onChange={(e) => setContainerName(e.target.value)}
-          />
+          /> */}
           <input
             type="text"
             placeholder="Port (default: 3000)"
@@ -68,7 +87,10 @@ services:
             value={TAGS}
             onChange={(e) => setTags(e.target.value)}
           />
-          <button onClick={handleDownload}>Generate Docker Compose</button>
+          <button onClick={handleAddContainer}>Add Container</button>
+          {/* <button onClick={handleGenerateCompose}>
+            Generate Docker Compose
+          </button> */}
         </div>
 
         {dockerComposeConfig && (
@@ -97,6 +119,18 @@ services:
             </a>
           </div>
         )}
+
+        {/* <div className="container-entries">
+          <h2>Container Entries</h2>
+          <ul>
+            {containerEntries.map((entry, index) => (
+              <li key={index}>
+                Container Name: {entry.containerName}, Port: {entry.port},
+                HIVE_ID: {entry.HIVE_ID}, TAGS: {entry.TAGS}
+              </li>
+            ))}
+          </ul>
+        </div> */}
       </div>
     </div>
   );
