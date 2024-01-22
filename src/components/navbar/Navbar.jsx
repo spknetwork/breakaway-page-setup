9bdbec93d08572d83a0047d2f3d95c1dfc01f32b
igormuba <igorm-uba@hotmail.com>
2024-01-22 12:50:35 -0400

    update navbar responsivity

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.scss";
 import { useSelector } from "react-redux";
@@ -7,6 +7,24 @@ import { FaBars } from "react-icons/fa";
 
 const Navbar = ({ toggleSidebar }) => {
   const { auth } = useSelector((state) => state);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="navbar">
@@ -16,8 +34,13 @@ const Navbar = ({ toggleSidebar }) => {
         </Link>
       </div>
       <div className="nav-links">
-        <Link to="/communities">Explore Communities</Link>
-        <Link to="/community-create">Create Community</Link>
+        <Link to="/communities">
+          {isSmallScreen ? "Explore" : "Explore Communities"}
+        </Link>
+        <Link to="/community-create">
+          {" "}
+          {isSmallScreen ? "Create" : "Create Community"}
+        </Link>
         <Link to="/docker-setup">Breakaway</Link>
         <Link to="/about">About</Link>
 
