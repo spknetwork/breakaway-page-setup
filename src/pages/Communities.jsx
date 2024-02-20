2d08608a13be9b678cf099d04c2c63efdda034a1
igormuba <igorm-uba@hotmail.com>
2024-02-20 19:32:57 -0300

    possibly no need to settimout there

--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -10,7 +10,7 @@ const Communities = () => {
   const [loading, setLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
 
-  const [selectedOption, setSelectedOption] = useState('Breakaway communities');
+  const [selectedOption, setSelectedOption] = useState("Breakaway communities");
   const [gridView, setGridView] = useState(false);
 
   const pinnedCommunitiesWebsties = {
@@ -23,29 +23,39 @@ const Communities = () => {
   const pinnedCommunities = ["hive-109272", "hive-115309", "hive-140169"];
 
   useEffect(() => {
-    setTimeout(()=> {
-      fetchCommunities();
-        },3000)
-      }, [searchQuery, selectedOption]);
-  
-      
+    fetchCommunities();
+  }, [searchQuery, selectedOption]);
+
   const handleSelectChange = async (event) => {
     const selectedValue = event.target.value;
     setSelectedOption(selectedValue);
-  
-    if (selectedValue === 'Breakaway communities') {
-      const filteredCommunities = communityLists.filter((c) => pinnedCommunities.includes(c.name));
+
+    if (selectedValue === "Breakaway communities") {
+      const filteredCommunities = communityLists.filter((c) =>
+        pinnedCommunities.includes(c.name)
+      );
       setCommunityLists(filteredCommunities);
     } else {
-      await getCommunities("", 100, searchQuery || null,  event.target.value, "");
+      await getCommunities(
+        "",
+        100,
+        searchQuery || null,
+        event.target.value,
+        ""
+      );
     }
   };
-      
 
   const fetchCommunities = async () => {
     setLoading(true);
     try {
-      const communities = await getCommunities("", 100, searchQuery || null,  "rank", "");
+      const communities = await getCommunities(
+        "",
+        100,
+        searchQuery || null,
+        "rank",
+        ""
+      );
 
       const pinnedCommunitiesData = await Promise.all(
         pinnedCommunities.map(async (communityId) => {
@@ -66,12 +76,15 @@ const Communities = () => {
         })
       );
 
-      const mergedCommunities = (!searchQuery
-        ? [...pinnedCommunitiesData, ...communities]
-        : [...communities]) || []
+      const mergedCommunities =
+        (!searchQuery
+          ? [...pinnedCommunitiesData, ...communities]
+          : [...communities]) || [];
 
-      if (selectedOption === 'Breakaway communities') {
-        const filteredCommunities = mergedCommunities.filter((c) => pinnedCommunities.includes(c.name));
+      if (selectedOption === "Breakaway communities") {
+        const filteredCommunities = mergedCommunities.filter((c) =>
+          pinnedCommunities.includes(c.name)
+        );
         setCommunityLists(filteredCommunities);
       } else {
         setCommunityLists(mergedCommunities);
@@ -90,104 +103,115 @@ const Communities = () => {
 
   return (
     <div className="communities setup">
-    <div className="community-header">
-      <h1>Tokenized Breakaway Communities</h1>
-    </div>
-    <div className="search-container">
-      <input
-        className="communities-search"
-        value={searchQuery}
-        type="text"
-        placeholder="Search community"
-        onChange={handleCommunitySearch}
-      />
-    </div>
-    {loading ? (
-      <div className="communities-container">
-        <Loader />
+      <div className="community-header">
+        <h1>Tokenized Breakaway Communities</h1>
       </div>
-    ) : communityLists.length > 0 ? (
-      <div className="communities-container">
-        <div className="view">
-          <span>{gridView ? listView : gridIcon}</span>
-          <h3 onClick={() => setGridView(!gridView)}>{gridView ? "List view" : "Grid view"}</h3>
+      <div className="search-container">
+        <input
+          className="communities-search"
+          value={searchQuery}
+          type="text"
+          placeholder="Search community"
+          onChange={handleCommunitySearch}
+        />
+      </div>
+      {loading ? (
+        <div className="communities-container">
+          <Loader />
         </div>
-        <div className="community">
-          <div className="select-communities">
-            <select
-              name="communities"
-              id="communities"
-              value={selectedOption}
-              onChange={handleSelectChange}
-            >
-              <option value="Breakaway communities">Breakaway communities</option>
-              <option value="rank">Rank</option>
-              <option value="new">New</option>
-              <option value="subs">Members</option>
-            </select>
+      ) : communityLists.length > 0 ? (
+        <div className="communities-container">
+          <div className="view">
+            <span>{gridView ? listView : gridIcon}</span>
+            <h3 onClick={() => setGridView(!gridView)}>
+              {gridView ? "List view" : "Grid view"}
+            </h3>
           </div>
-          <div className="community-main">
-            {communityLists.map((c, i) => (
-              <div
-                className={`${gridView ? "community-wrapper-grid" : "community-wrapper-list"}
+          <div className="community">
+            <div className="select-communities">
+              <select
+                name="communities"
+                id="communities"
+                value={selectedOption}
+                onChange={handleSelectChange}
+              >
+                <option value="Breakaway communities">
+                  Breakaway communities
+                </option>
+                <option value="rank">Rank</option>
+                <option value="new">New</option>
+                <option value="subs">Members</option>
+              </select>
+            </div>
+            <div className="community-main">
+              {communityLists.map((c, i) => (
+                <div
+                  className={`${
+                    gridView
+                      ? "community-wrapper-grid"
+                      : "community-wrapper-list"
+                  }
                     community-wrapper${c.isPinned ? " pinned-community" : ""}
                     `}
-                key={i}
-              >
-                <div className="left grid-top">
-                  <div className="top">
-                    <img src={`https://images.hive.blog/u/${c.name}/avatar`} alt="" />
-                    <Link to={`/community/hive-${c.id}`}>{c.title}</Link>
-                  </div>
-                  <div className="bottom">
-                    <span className="about">{c.about}</span>
-                    <div className="community-info">
-                      <span>{c.subscribers} members</span>
-                      <span>|</span>
-                      <span>{c.num_pending} Posts</span>
-                      <span>|</span>
-                      <span>{c.num_authors} posters</span>
+                  key={i}
+                >
+                  <div className="left grid-top">
+                    <div className="top">
+                      <img
+                        src={`https://images.hive.blog/u/${c.name}/avatar`}
+                        alt=""
+                      />
+                      <Link to={`/community/hive-${c.id}`}>{c.title}</Link>
                     </div>
-                    <div className="admins-wrapper">
-                      <span>Admin:</span>
-                      <div className="admins">
-                        {c?.admins?.map((admin, i) => (
-                          <div key={i} className="each-admin">
-                            <span className="admin">@{admin}</span>
-                          </div>
-                        ))}
+                    <div className="bottom">
+                      <span className="about">{c.about}</span>
+                      <div className="community-info">
+                        <span>{c.subscribers} members</span>
+                        <span>|</span>
+                        <span>{c.num_pending} Posts</span>
+                        <span>|</span>
+                        <span>{c.num_authors} posters</span>
+                      </div>
+                      <div className="admins-wrapper">
+                        <span>Admin:</span>
+                        <div className="admins">
+                          {c?.admins?.map((admin, i) => (
+                            <div key={i} className="each-admin">
+                              <span className="admin">@{admin}</span>
+                            </div>
+                          ))}
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <div className="right">
+                    {c.isPinned ? (
+                      <button
+                        onClick={() =>
+                          window.open(
+                            `${pinnedCommunitiesWebsties[c.name]}`,
+                            "_blank"
+                          )
+                        }
+                      >
+                        Visit platform
+                      </button>
+                    ) : (
+                      <Link to="/docker-setup">
+                        Start your platform for this community
+                      </Link>
+                    )}
+                  </div>
                 </div>
-                <div className="right">
-                  {c.isPinned ? (
-                    <button
-                      onClick={() =>
-                        window.open(
-                          `${pinnedCommunitiesWebsties[c.name]}`,
-                          "_blank"
-                        )
-                      }
-                    >
-                      Visit platform
-                    </button>
-                  ) : (
-                    <Link to="/docker-setup">
-                      Start your platform for this community
-                    </Link>
-                  )}
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    ) : (
-      <div className="communities-container">No community found</div>
-    )}
-  </div>
-  )
-}
+      ) : (
+        <div className="communities-container">No community found</div>
+      )}
+    </div>
+  );
+};
 
 export default Communities;
